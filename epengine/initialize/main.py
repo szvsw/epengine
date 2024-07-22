66dd8d006ed0ef71835f0cfd9495bc515480526f
Sam Wolk <36545842+szvsw@users.noreply.github.com>
2024-07-22 13:48:47 -0400

    add local moto support

---
+++ epengine/initialize/main.py
@@ -0,0 +1,21 @@
+import logging
+import os
+
+import boto3
+
+s3 = boto3.client("s3")
+
+logger = logging.getLogger()
+logging.basicConfig(level=logging.INFO)
+logger.setLevel(logging.INFO)
+
+# list buckets
+if __name__ == "__main__":
+    bucket_name = "ml-for-bem"
+    # create the bucket if it does not exist
+    try:
+        region = os.getenv("AWS_DEFAULT_REGION")
+        s3.create_bucket(Bucket=bucket_name, CreateBucketConfiguration={"LocationConstraint": region})
+        logger.info(f"Bucket {bucket_name} created")
+    except Exception as e:
+        logger.info(f"Bucket {bucket_name} already exists, {e}")
