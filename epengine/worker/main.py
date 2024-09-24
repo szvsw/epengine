fa3655f2c77e575ed0054cdfdb62eaf021f6233d
Sam Wolk <36545842+szvsw@users.noreply.github.com>
2024-09-24 18:03:15 -0400

    add coiled to dockerfile

--- epengine/worker/main.py
+++ epengine/worker/main.py
@@ -5,7 +5,11 @@ import os
 from pydantic_settings import BaseSettings
 
 from epengine.hatchet import hatchet
-from epengine.workflows import ScatterGatherRecursiveWorkflow, ScatterGatherWorkflow, Simulate
+from epengine.workflows import (
+    ScatterGatherRecursiveWorkflow,
+    ScatterGatherWorkflow,
+    Simulate,
+)
 
 
 class SimWorkerSettings(BaseSettings):
@@ -29,7 +33,9 @@ class SimWorkerSettings(BaseSettings):
         """Return the name of the worker."""
         base = "EnergyPlusWorker"
         hosting = "AWS" if self.in_aws else ("Fly" if self.in_fly else "Local")
-        hosting_with_region = f"{hosting}{self.FLY_REGION.upper()}" if self.FLY_REGION else hosting
+        hosting_with_region = (
+            f"{hosting}{self.FLY_REGION.upper()}" if self.FLY_REGION else hosting
+        )
         max_runs = self.max_runs
         return f"{base}--{hosting_with_region}--{max_runs:03d}slots"
 
@@ -39,6 +45,39 @@ class SimWorkerSettings(BaseSettings):
         cpu_ct = os.cpu_count() or 1
         return max(1, cpu_ct - 1)
 
+    def make_worker(self):
+        """Create a worker with the settings.
+
+        Handles registering the appropriate workflows based on the settings.
+
+        Returns:
+            Worker: The worker.
+
+        """
+        worker = hatchet.worker(
+            self.name,
+            max_runs=self.max_runs,
+        )
+
+        if self.FLY_REGION == "sea" or self.FLY_REGION is None:
+            worker.register_workflow(ScatterGatherWorkflow())
+            worker.register_workflow(ScatterGatherRecursiveWorkflow())
+
+        worker.register_workflow(Simulate())
+
+        return worker
+
+
+async def arun():
+    """Run the EnergyPlus worker.
+
+    Note that this function will be blocking.
+    """
+    settings = SimWorkerSettings()
+    worker = settings.make_worker()
+
+    await worker.async_start()
+
 
 def run():
     """Run the EnergyPlus worker.
@@ -46,19 +85,7 @@ def run():
     Note that this function will be blocking.
     """
     settings = SimWorkerSettings()
-    max_runs = settings.max_runs
-
-    worker = hatchet.worker(
-        settings.name,
-        max_runs=max_runs,
-    )
-
-    if settings.FLY_REGION == "sea" or settings.FLY_REGION is None:
-        worker.register_workflow(ScatterGatherWorkflow())
-        worker.register_workflow(ScatterGatherRecursiveWorkflow())
-
-    worker.register_workflow(Simulate())
-
+    worker = settings.make_worker()
     worker.start()
 
 
